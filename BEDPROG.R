06a6a0eb263c9294eb7e5921130e256456365066
Sigurjón Helgi Björnsson <shbj@nanoq.gl>
2020-09-22 15:47:41 -0200

    new line

--- BEDPROG.R
+++ BEDPROG.R
@@ -1,12 +1,12 @@
 rm(list = ls())
 options(scipen=999)
+
 library(pxweb)
 library(tidyverse)
 library("devtools")
 devtools::install_github("ropengov/pxweb")
 library(sqldf)
 library(xlsx)
-###
 ###################################################################################################
 # 
 #   Based on historical data from 1977 to 2019 and projection from 2020 to 2050
@@ -25,6 +25,7 @@ library(xlsx)
 #     t          -> År                                                         : numeric
 #     faar       -> fødeår    :1878:2040                                       : numeric
 #     nb_lag     -> skal tjekkes                                               : integer
+#
 #
 ####################################################################################################
 #
@@ -54,25 +55,24 @@ Cutin    <-2020
 ## GS table of projection done in 2018    (kemr øryggisstadlar)
 
 
-#
-#BEXP_2018 = "http://bank.stat.gl/api/v1/en/Greenland/BE/BE01/BE0150/BEXP18.px"
-BEXP_2020_f = "http://bank.stat.gl/api/v1/da/Greenland/BE/BE01/BE0150/BEXP20.PX"
+BEXP_2018 = "http://bank.stat.gl/api/v1/en/Greenland/BE/BE01/BE0150/BEXP18.px"
+
 # Historical data where we get registered to 2019
 
-BEXP_2020_h = "http://bank.stat.gl/api/v1/en/Greenland/BE/BE01/BE0120/BEXST1.PX"
+BEXP_2019 = "http://bank.stat.gl/api/v1/en/Greenland/BE/BE01/BE0120/BEXST1.PX"
 
 
-## Varibels in tabels need 2 list due to not same name convention which is lousy discipline by GS checj
+## Varibels in tabels need 2 list due to not same name convention which is lousy discipline by GS
 
 
-Var_List_2020_f <- list("version"=c("*"),
+Var_List_2018 <- list("version"=c("*"),
                     "place of birth"=c("*"),
                     "age"=c("*"),
                     "sex"=c("*"),
                     "time"=c("*"))
 ############################################################################
 
-Var_List_2020_h <- list("residence"=c("*"),
+Var_List_2019 <- list("residence"=c("*"),
                  "place of birth"=c("*"),
                  "gender"=c("*"),
                  "age"=c("*"),
@@ -80,45 +80,45 @@ Var_List_2020_h <- list("residence"=c("*"),
 
 ##############################################################################
 
-pxq_2020_f <- pxweb_query(Var_List_2020_f)
-pxq_2020_h <- pxweb_query(Var_List_2020_h)
+pxq_2018 <- pxweb_query(Var_List_2018)
+pxq_2019 <- pxweb_query(Var_List_2019)
 
 ##############################################################################
 
-tabel_2020_f <- pxweb_get(url =BEXP_2020_f,pxq_2020_f)
-tabel_2020_h <- pxweb_get(url =BEXP_2020_h,pxq_2020_h)
+tabel_2018 <- pxweb_get(url =BEXP_2018,pxq_2018)
+tabel_2019 <- pxweb_get(url =BEXP_2019,pxq_2019)
 
-BeProg_2020_f<- as.data.frame(tabel_2020_f,column.name.type = "code", variable.value= "code",stringsAsFactors = FALSE)
-BeProg_2020_h<- as.data.frame(tabel_2020_h,column.name.type = "code", variable.value= "code",stringsAsFactors = FALSE)
+BeProg_2018<- as.data.frame(tabel_2018,column.name.type = "code", variable.value= "code",stringsAsFactors = FALSE)
+BeProg_2019<- as.data.frame(tabel_2019,column.name.type = "code", variable.value= "code",stringsAsFactors = FALSE)
 
 
 ## We choice 2018 main alternative
 
-BeProg_2020_f<-BeProg_2020_f%>%filter(version=="2020 main alternative")
+BeProg_2018<-BeProg_2018%>%filter(version=="2018 main alternative")
 
 ##  Convert to right format 
 
-FinalBeprog<-BeProg_2020_f%>%filter(time>=Cutin)%>%transmute(sex,alder=as.numeric(age),t=as.numeric(time),nb=`Befolkningsfremskrivning 2020`,birthplace=if_else(`place of birth`=="Born in Greenland","Greenland","Total"))
+FinalBeprog<-BeProg_2018%>%filter(time>=Cutin)%>%transmute(sex,alder=as.numeric(age),t=as.numeric(time),nb=`Populationforecast 2018`,birthplace=if_else(`place of birth`=="Born in Greenland","Greenland","Total"))
 
 ## Cutoff the forecast
 
-FinalBeprog_2020_f<-FinalBeprog%>%filter(t<=Cutoff)                                                                                     
+FinalBeprog_2018<-FinalBeprog%>%filter(t<=Cutoff)                                                                                     
 
 
 ## Get the historical data
 
 ## Take Total
-BeProg_2020_h<-BeProg_2020_h%>%filter(residence=="Total")
+BeProg_2019<-BeProg_2019%>%filter(residence=="Total")
 
 ## Make formatering
 
-BeProg_2020_h<-BeProg_2020_h%>%transmute(sex=gender,alder=as.numeric(age),t=as.numeric(time),nb=`Population January 1st`,birthplace=`place of birth`)
-BeProg_2020_h<-BeProg_2020_h%>%filter(sex%in%c("Men","Women"))
-FinalBeprog_2020_h<-BeProg_2020_h%>%filter(birthplace%in%c("Greenland","Total"))
+BeProg_2019<-BeProg_2019%>%transmute(sex=gender,alder=as.numeric(age),t=as.numeric(time),nb=`Population January 1st`,birthplace=`place of birth`)
+BeProg_2019<-BeProg_2019%>%filter(sex%in%c("Men","Women"))
+FinalBeprog_2019<-BeProg_2019%>%filter(birthplace%in%c("Greenland","Total"))
 
 ## Combine the historical and the 
 
-FinalBeprog<-rbind(FinalBeprog_2020_h,FinalBeprog_2020_f)
+FinalBeprog<-rbind(FinalBeprog_2019,FinalBeprog_2018)
 
 
 
@@ -128,16 +128,16 @@ FinalBeprog<-rbind(FinalBeprog_2020_h,FinalBeprog_2020_f)
 
 FinalBeprog$faar <- FinalBeprog$t - FinalBeprog$alder
 
-FinalBe_2020<-FinalBeprog %>% arrange(sex,birthplace,faar,t) %>% group_by(sex,birthplace,faar, add = F) %>% mutate(nb_lag = lag(nb))%>%ungroup()
+FinalBe_2019<-FinalBeprog %>% arrange(sex,birthplace,faar,t) %>% group_by(sex,birthplace,faar, add = F) %>% mutate(nb_lag = lag(nb))%>%ungroup()
 
 ## It is possible to extract danish but some issue therefore use the english version and convert to danish 
 ## names. Here is small letter for women (kvinder)
 
-d2<-FinalBe_2020%>%transmute(køn=factor(if_else(sex=="Men","mænd","kvinder")),alder=as.integer(alder),fødested=
+d2<-FinalBe_2019%>%transmute(køn=factor(if_else(sex=="Men","mænd","kvinder")),alder=as.integer(alder),fødested=
                                                factor(if_else(birthplace=="Total","Hele befolkningen","Personer født i Grønland")),nb=as.integer(nb),t,faar,nb_lag=as.integer(nb_lag))
 
 
-d2<-FinalBe_2020%>%transmute(køn=factor(if_else(sex=="Men","mænd","kvinder")),alder=as.integer(alder),fødested=
+d2<-FinalBe_2019%>%transmute(køn=factor(if_else(sex=="Men","mænd","kvinder")),alder=as.integer(alder),fødested=
                                factor(if_else(birthplace=="Total","Hele befolkningen","Personer født i Grønland")),nb=as.integer(nb),t,faar,nb_lag=as.integer(nb_lag))
 
 
